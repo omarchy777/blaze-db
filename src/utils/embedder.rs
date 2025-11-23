908b2e2cbac7374f68e6b07cb35e9895535d85bf
Rounak Gh <ronakgh97@gmail.com>
2025-11-23 12:10:20 +0530

    refactor embedder module for improved data structure and error handling

--- src/utils/embedder.rs
+++ src/utils/embedder.rs
@@ -10,44 +10,48 @@ pub struct Embeddings {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct EmbeddingData {
-    pub embedding: Vec<f64>,
     pub index: i32,
+    #[serde(skip_deserializing)]
+    pub chunk: String,
+    pub embedding: Vec<f64>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct EmbeddingJson {
-    pub chucks: Vec<String>,
-    pub embeddings: Embeddings,
+    pub batch_index: usize,
+    pub items: Vec<EmbeddingData>,
 }
 
 impl EmbeddingJson {
-    pub fn pretty_print(&self) {
-        for (i, embedding_data) in self.embeddings.data.iter().enumerate() {
-            println!(
-                "Chunk: {}\nEmbedding Index: {}\nEmbedding Vector (First three index): {:?}\n",
-                self.chucks[i],
-                embedding_data.index,
-                &embedding_data.embedding[..3] // Print first 3 values for brevity
-            );
+    pub fn new(batch_index: usize, items: Embeddings) -> Self {
+        Self {
+            batch_index,
+            items: items.data,
         }
     }
-}
-
-#[derive(Serialize, Deserialize)]
-pub struct RequestBody {
-    pub model: String,
-    pub input: Vec<String>,
+    pub fn pretty_print(&self) {
+        println!("Batch Index: {}", self.batch_index);
+        self.items.iter().take(3).for_each(|item| {
+            println!(
+                "Index: {:?}\n Chunk: {:?}\n Embeddings: {:?}\n Embedding Length: {:?}\n",
+                &item.index,
+                &item.chunk,
+                &item.embedding[..3],
+                &item.embedding.len(),
+            );
+        })
+    }
 }
 
 pub async fn fetch_embeddings_from_provider(
     url: &str,
     model: &str,
     chunks: Vec<&String>,
-) -> Result<EmbeddingJson, Error> {
-    let body = RequestBody {
-        model: model.to_string(),
-        input: chunks.into_iter().map(|s| s.to_string()).collect(),
-    };
+) -> Result<Embeddings, Error> {
+    let body = serde_json::json!({
+        "model": model,
+        "input": chunks,
+    });
 
     let response = reqwest::Client::new().post(url).json(&body).send().await?;
 
@@ -58,9 +62,27 @@ pub async fn fetch_embeddings_from_provider(
         )));
     }
 
-    let embeddings_response: Embeddings = response.json().await?;
+    let mut embeddings_response: Embeddings = response.json().await?;
+
+    embeddings_response
+        .data
+        .iter_mut()
+        .enumerate()
+        .for_each(|(index, embedding)| {
+            if let Some(chunk) = chunks.get(index) {
+                embedding.chunk = chunk.to_string();
+            } else {
+                embedding.chunk = String::from("");
+            }
+        });
 
     embeddings_response.data.par_iter().for_each(|embedding| {
+        if embedding.index.is_negative() {
+            eprintln!(
+                "Warning: Received embedding with -ve index for chunk: {}",
+                embedding.chunk
+            );
+        }
         if embedding.embedding.is_empty() {
             eprintln!(
                 "Warning: Received empty embedding for chunk index {}",
@@ -69,8 +91,5 @@ pub async fn fetch_embeddings_from_provider(
         }
     });
 
-    Ok(EmbeddingJson {
-        chucks: body.input,
-        embeddings: embeddings_response,
-    })
+    Ok(embeddings_response)
 }
