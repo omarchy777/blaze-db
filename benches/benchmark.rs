5f61acb27229e06d0062363a595973bd80c20c16
Rounak Gh <ronakgh97@gmail.com>
2025-11-24 18:16:19 +0530

    Refactor benchmark setup to initialize Ingestor outside of the benchmark iteration

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -2,10 +2,9 @@ use bencher::{Bencher, benchmark_group, benchmark_main};
 use blaze_db::utils::ingestor::Ingestor;
 
 fn my_bench(b: &mut Bencher) {
+    let ingest = Ingestor::new("./sample/War_and_peace.txt".into(), 512);
     b.iter(|| {
-        let ingest = Ingestor::new("./sample/War_and_peace.txt".into(), 512);
         Ingestor::read_line(&ingest).expect("Bad Thing");
-        // code to test
     });
 }
 
