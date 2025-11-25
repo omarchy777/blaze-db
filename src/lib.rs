9adb5681edf40c8ee0746c5b6b79fed765318d33
Rounak Gh <ronakgh97@gmail.com>
2025-11-25 21:18:40 +0530

    Add VectorData to prelude for improved accessibility in modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
+mod cli;
 mod core;
 mod utils;
 
 pub mod prelude {
-    pub use crate::utils::{EmbeddingStore, Ingestor, Provider};
+    pub use crate::utils::{EmbeddingStore, Ingestor, Provider, VectorData};
 }
