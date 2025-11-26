3c51e494454e274909a85a9239acffb1fc319c0c
Rounak Gh <ronakgh97@gmail.com>
2025-11-26 11:48:37 +0530

    Add benchmarking functions for Ingestor and EmbeddingStore; implement async loading of embeddings

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,12 +1,22 @@
 use bencher::{Bencher, benchmark_group, benchmark_main};
-use blaze_db::prelude::Ingestor;
+use blaze_db::prelude::{EmbeddingStore, Ingestor};
+use tokio::runtime::Runtime;
 
-fn my_bench(b: &mut Bencher) {
+fn read(b: &mut Bencher) {
     let ingest = Ingestor::new("./sample/War_and_peace.txt", 512);
     b.iter(|| {
         Ingestor::read_line(&ingest).expect("Bad Thing");
     });
 }
 
-benchmark_group!(benches, my_bench);
+fn load(b: &mut Bencher) {
+    let rt = Runtime::new().unwrap();
+    b.iter(|| {
+        rt.block_on(async {
+            let _data = EmbeddingStore::read_binary("./embeddings").await;
+        })
+    });
+}
+
+benchmark_group!(benches, read, load);
 benchmark_main!(benches);
